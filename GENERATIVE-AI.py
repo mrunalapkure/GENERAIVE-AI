972194c69f698e6284984544104b8bc1504fb9bc
Mrunal Apkure <mrunalapkure@gmail.com>
2025-06-29 12:40:08 +0530

    Create GENERATIVE-AI.py

---
+++ GENERATIVE-AI.py
@@ -0,0 +1,15 @@
+import google.generativeai as genai
+
+API_KEY = "AIzaSyDfrsniSGT0ZResrRJz6yNE1gFx4u4Exi4"
+genai.configure(api_key=API_KEY)
+
+model = genai.GenerativeModel("gemini-2.0-flash")
+chat = model.start_chat()
+
+print("chat with Gemini! Type 'exit' to quit.")
+while True:
+    user_input = input("you: ")
+    if user_input.lower() == 'exit':
+        break
+    response = chat.send_message(user_input) 
+    print("Gemini:",response.text)
